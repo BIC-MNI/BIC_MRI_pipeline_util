b839b3c8269ee8f6c7adfadd4a08282a3cb43e9a
Maxime Mulder - B.I.C. Staff <maxime-mulder@outlook.com>
2025-05-14 00:06:34 -0400

    add config read check

--- src/bic_util/config.py
+++ src/bic_util/config.py
@@ -2,6 +2,7 @@ import importlib.util
 import os
 from typing import Any
 
+from bic_util.fs import require_readable_file
 from bic_util.print import print_error_exit
 
 
@@ -17,6 +18,8 @@ def load_config_module(file_name: str) -> Any:
     config_file_path = os.path.join(config_fir_path, file_name)
     if not os.path.exists(config_file_path):
         print_error_exit(f"Configuration file '{file_name}' not found in the configuration directory.")
+
+    require_readable_file(config_file_path)
 
     specification = importlib.util.spec_from_file_location('config', config_file_path)
     if specification is None or specification.loader is None:
